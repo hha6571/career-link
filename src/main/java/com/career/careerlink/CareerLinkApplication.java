e4ddddce5d6a875357e39cd932672a524592946a
hayun0502 <hayun.dev00@gmail.com>
2025-06-24 13:04:00 +0900

    first commit : initial Spring Boot Project

---
+++ src/main/java/com/career/careerlink/CareerLinkApplication.java
@@ -0,0 +1,13 @@
+package com.career.careerlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/career/careerlink/CareerLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.career.careerlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
