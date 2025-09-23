f43abfd6e81ec7664b34bb81a802466665c6f3f7
JIHEE <jhkimm96@gmail.com>
2025-09-23 15:58:39 +0900

    config : setAllowedOrigins 추가

--- src/main/java/com/career/careerlink/global/config/SecurityConfig.java
+++ src/main/java/com/career/careerlink/global/config/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // React 앱 주소
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","https://careerlink.online")); // React 앱 주소
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
